2c6842f60578a88722b00371d1715b8257c3cc48
rafaaelalves <33187054+rafaaelalves@users.noreply.github.com>
2020-05-06 09:33:19 -0300

    Add files via upload

--- ficha2.js
+++ ficha2.js
@@ -72,9 +72,7 @@ piloto.reacoes.psique = 10 + piloto.atributos.inteligencia + piloto.geral.profic
 piloto.reacoes.protecaocinetica = 10 + piloto.reacoes.armadura[0] + piloto.reacoes.escudo[0],
 piloto.reacoes.protecaoenergia = 10 + piloto.reacoes.armadura[1] + piloto.reacoes.escudo[1]
 
-// console.log(piloto.geral)
-// console.log('------- Piloto fim -------')
-
+//----------------------------------------------------------------------------------------------------------------
 const mech = {
     geral: {
         nível: 1,
@@ -106,7 +104,7 @@ const mech = {
             maxSubunits: 6
         },
         
-        braços: {
+        bracos: {
             ap: 10,
             protecaoCinetica: 20,
             protecaoEnergia: 10,
@@ -163,19 +161,20 @@ mech.geral.carryweight = mech.atributos.forca *150,
 //mech.geral.pontosdevida = mech.geral.dadosdevida * mech.atributos.constuicao,
 
 mech.reacoes.iniciativa = mech.atributos.destreza,
-mech.reacoes.percepcaopassiva = mech.atributos.sabedoria + piloto.geral.proficiencia,
+//mech.reacoes.percepcaopassiva = mech.atributos.sabedoria + piloto.geral.proficiencia, //este item deve ser excluido ou deve ser tirado do piloto
 mech.reacoes.reflexos = 10 + mech.atributos.destreza + piloto.geral.proficiencia,
 mech.reacoes.vitalidade = 10 + mech.atributos.constuicao + piloto.geral.proficiencia,
-mech.reacoes.psique = 10 + mech.atributos.inteligencia + piloto.geral.proficiencia, //a psique do mech se refere a proteção contra jamming, hacking, etc
+mech.reacoes.psique = 10 + mech.atributos.inteligencia + piloto.geral.proficiencia //a psique do mech se refere a proteção contra jamming, hacking, etc
 //mech.reacoes.protecaocinetica = 10 + mech.reacoes.armadura[0] + mech.reacoes.escudo[0],
 //mech.reacoes.protecaoenergia = 10 + mech.reacoes.armadura[1] + mech.reacoes.escudo[1]
 
-//console.log(mech.geral)
+console.log(mech.componentes.cabeca.hasOwnProperty('ap'))
+console.log(mech.componentes.cabeca.ap)
 
-apTotal = 0
-for (let i in mech.componentes){
-    if (ap in mech.componentes[i]){
-        apTotal += mech.componentes[i].ap
-        console.log.log(apTotal)
+for (let item in mech.componentes){
+    if (item.hasOwnProperty('ap')){
+        console.log(item.ap)
+    }else {
+        console.log(item, 'não possui ap')
     }
 }
